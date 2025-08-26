1fb854b588b0d3d3ee8726c28247e67a96043615
Joe Robinson <joerob@microsoft.com>
2025-08-26 14:25:12 -0700

    testing

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddControllers();
+// Temporarily comment out controllers to test
+// builder.Services.AddControllers();
 
 // Register certificate services based on environment
 var isAppService = !string.IsNullOrEmpty(Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME"));
@@ -26,24 +27,24 @@ else
     builder.Services.AddSingleton<ICertificateService, CertificateService>();
 }
 
-// Add API documentation services
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new() { 
-        Title = "Azure Certificate Inventory API", 
-        Version = "v1",
-        Description = "API for managing and retrieving certificate information from Azure App Service and local certificate stores."
-    });
+// Add API documentation services - temporarily commented out
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen(c =>
+// {
+//     c.SwaggerDoc("v1", new() { 
+//         Title = "Azure Certificate Inventory API", 
+//         Version = "v1",
+//         Description = "API for managing and retrieving certificate information from Azure App Service and local certificate stores."
+//     });
     
-    // Include XML comments for better documentation
-    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    if (File.Exists(xmlPath))
-    {
-        c.IncludeXmlComments(xmlPath);
-    }
-});
+//     // Include XML comments for better documentation
+//     var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+//     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+//     if (File.Exists(xmlPath))
+//     {
+//         c.IncludeXmlComments(xmlPath);
+//     }
+// });
 
 var app = builder.Build();
 
@@ -54,16 +55,16 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios.
     app.UseHsts();
 }
-else
-{
-    // Enable Swagger in development
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Azure Certificate Inventory API v1");
-        c.RoutePrefix = "swagger";
-    });
-}
+// else
+// {
+//     // Enable Swagger in development
+//     app.UseSwagger();
+//     app.UseSwaggerUI(c =>
+//     {
+//         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Azure Certificate Inventory API v1");
+//         c.RoutePrefix = "swagger";
+//     });
+// }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -74,7 +75,7 @@ app.UseAuthorization();
 
 app.MapRazorPages();
 
-// Simple approach: don't use any complex routing, just basic controllers
-app.MapControllers();
+// Temporarily comment out controllers to test if they're causing the conflict
+// app.MapControllers();
 
 app.Run();
